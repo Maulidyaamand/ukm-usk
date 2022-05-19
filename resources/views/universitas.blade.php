dbad64f8156d00bf0cbc28b61a79224c2c8934ea
maulidya amanda <lidya_ap@mhs.unsyiah.ac.id>
2022-05-19 18:26:53 +0700

    comit kedua

---
+++ resources/views/universitas.blade.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>UKM-USK</title>
+    <link rel="stylesheet" type="text/css" href="../public/main.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+        integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+</head>
+
+<body>
+
+    <header>
+        <div class="navbar">
+            <h1><a href="index.html">UKM</a></h1>
+
+            <ul>
+                <li class="active"><a href="#">Beranda</a></li>
+                <li><a href="#">Daftar Kegiatan</a>
+                    <div class="sub-menu-1">
+                        <ul>
+                            <li class="hover-me"><a href="#">Universitas</a><i class="fa fa-angle-right"></i>
+
+                                <div class="sub-menu-2">
+                                    <ul>
+                                        <li><a href="#">Agama</a></li>
+                                        <li><a href="#">Budaya</a></li>
+                                        <li><a href="#">Olahraga</a></li>
+                                        <li><a href="#">Sosial</a></li>
+                                    </ul>
+                                </div>
+
+                            </li>
+                            <li class="hover-me"><a href="#">Fakultas</a><i class="fa fa-angle-right"></i>
+                                <div class="sub-menu-2">
+                                    <ul>
+                                        <li><a href="#">FKIP</a></li>
+                                        <li><a href="#">FMIPA</a></li>
+                                        <li><a href="#">Hukum</a></li>
+                                        <li><a href="#">Teknik</a></li>
+                                    </ul>
+
+
+                                </div>
+
+                            </li>
+                        </ul>
+                    </div>
+
+                </li>
+                <li><a href="#">Berita Terkini</a></li>
+                <li><a href="#">Pendaftaran</a></li>
+                <li><a href="#">Kontak</a></li>
+            </ul>
+
+        </div>
+    </header>
+
+    <!--bener-->
+    <div class="banner">
+        <div class="container">
+            <h2>UNIT KEGIATAN MAHASISWA</h2>
+        </div>
+    </div>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/universitas', function () {
+    return view('universitas');
+});
+
+Route::get('/kontak', function () {
+    return view('kontak');
+});
+
